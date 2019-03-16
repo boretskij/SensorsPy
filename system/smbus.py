e4ac22e3a27561a849d1dbd8a320146e5f9f8873
Олександр Загарія <zagariya@univ.net.ua>
2019-03-16 13:02:03 +0200

    Added whitelist

--- system/smbus.py
+++ system/smbus.py
@@ -18,14 +18,19 @@ class SMBus:
                 self.bus.append(num)
         return self.bus
     
-    def detect_all_devices(self):
+    def detect_all_devices(self,whitelist=[]):
         if (len(self.bus)==0):
             self.detect_all_i2c()
+        
+        devices = range(128)
+        
+        if (len(whitelist)>0):
+            devices = whitelist
         
         for bus_num in self.bus:
             self.sensors = {bus_num:[]}
             bus = smbus.SMBus(bus_num)
-            for device in range(128):
+            for device in devices:
                 try:
                     bus.read_byte(device)
                 except:
